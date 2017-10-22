e4d923dc97a69cfd646cfa653d876cc2d7d51d59
natalia navas <natinavas@gmail.com>
2017-10-22 16:48:39 -0300

    Add butter filter
    -Raise exception when video file could not be opened
    -TODO: test filter

--- sample/heartrate.py
+++ sample/heartrate.py
@@ -7,17 +7,17 @@ Created on Sat Sep 16 19:23:10 2017
 
 import numpy as np
 import argparse
-from scipy.signal import butter, filtfilt
+from scipy.signal import butter, lfilter, lfilter_zi, filtfilt
 
 import matplotlib.pyplot as plt
 import cv2
 
 from fft.fft import FFT, FFT_shift
 
-# Read program arguments
 parser = argparse.ArgumentParser(description="Photoplesthysmography")
 parser.add_argument("--file", "-f", type=str, dest='file', default='/Users/natinavas/Documents/ITBA/MNA/photoplesthysmography/xid-120473_1.mp4')
 parser.add_argument("--butter_filter", "-bf", type=bool, dest='filter', default=False)
+
 args = parser.parse_args()
 
 cap = cv2.VideoCapture(args.file)
@@ -55,10 +55,23 @@ r = r[0,0:n]-np.mean(r[0,0:n])
 g = g[0,0:n]-np.mean(g[0,0:n])
 b = b[0,0:n]-np.mean(b[0,0:n])
 
-# Calculate fourier transformations
-R = np.abs(FFT_shift(FFT(r))) ** 2
-G = np.abs(FFT_shift(FFT(g))) ** 2
-B = np.abs(FFT_shift(FFT(b))) ** 2
+# B, A = butter(2, 0.12)
+# r = filtfilt(B, A, r)
+# g = filtfilt(B, A, g)
+# b = filtfilt(B, A, b)
+
+
+# zi = lfilter_zi(B,A)
+# r, _ = lfilter(B, A, r, zi=zi * r[0])
+# g, _ = lfilter(B, A, g, zi=zi * g[0])
+# b, _ = lfilter(B, A, b, zi=zi * b[0])
+# y = signal.filtfilt(b, a, xn)
+
+print(len(r))
+
+R = np.abs(FFT_shift(np.fft.fft(r)))**2
+G = np.abs(FFT_shift(np.fft.fft(g)))**2
+B = np.abs(FFT_shift(np.fft.fft(b)))**2
 
 # Use butter filter
 if args.filter:
