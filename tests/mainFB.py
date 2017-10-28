dbb9a8ad3952a0eeda48fa38eb8253ce6677f56b
natalia navas <natinavas@gmail.com>
2017-10-28 17:46:00 -0300

    Complete interval test

--- tests/mainFB.py
+++ tests/mainFB.py
@@ -2,49 +2,70 @@ import numpy as np
 import tests
 import argparse
 import cv2
+from scipy.signal import butter, filtfilt
 
-# parser = argparse.ArgumentParser(description="Photoplesthysmography")
-# parser.add_argument("--file", "-f", type=str, dest='file', default='/home/francis/Desktop/videodedo.mp4')
-#
-# args = parser.parse_args()
-#
-# cap = cv2.VideoCapture(args.file)
-#
-# if not cap.isOpened():
-#    print("No lo pude abrir")
-#
-# length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-# fps = cap.get(cv2.CAP_PROP_FPS)
-#
-# r = np.zeros((1, length))
-# g = np.zeros((1, length))
-# b = np.zeros((1, length))
-#
-# k = 0
-# while (cap.isOpened()):
-#     ret, frame = cap.read()
-#
-#     if ret == True:
-#         r[0, k] = np.mean(frame[330:360, 610:640, 0])
-#         g[0, k] = np.mean(frame[330:360, 610:640, 1])
-#         b[0, k] = np.mean(frame[330:360, 610:640, 2])
-#     # print(k)
-#     else:
-#         break
-#     k = k + 1
-#
-# cap.release()
-# cv2.destroyAllWindows()
-#
-# r = r[0,0:length]-np.mean(r[0,0:length])
-# g = g[0,0:length]-np.mean(g[0,0:length])
-# b = b[0,0:length]-np.mean(b[0,0:length])
-#
-# fps = 30
-# tests.interval_test(g,fps)
+parser = argparse.ArgumentParser(description="Photoplesthysmography")
+#parser.add_argument("--file", "-f", type=str, dest='file', default='/Users/natinavas/Documents/ITBA/MNA/photoplesthysmography/xid-120473_1.mp4')
+parser.add_argument("--file", "-f", type=str, dest='file', default='/Users/natinavas/Downloads/video4minSeba.MOV')
 
-vec = []
-vec = [vec, 4]
-print(vec)
+args = parser.parse_args()
+
+cap = cv2.VideoCapture(args.file)
+
+if not cap.isOpened():
+   print("No lo pude abrir")
+
+firstFrames = 50
+length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - firstFrames
+print(length)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(width)
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(height)
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+r = np.zeros((1, length))
+g = np.zeros((1, length))
+b = np.zeros((1, length))
+
+minX = 0
+maxX = 1000
+minY = 0
+maxY = 700
+
+k = 0
+while (cap.isOpened()):
+    ret, frame = cap.read()
+
+    if ret == True :
+        if k >= firstFrames:
+            r[0, k - firstFrames] = np.mean(frame[minX:maxX, minY:maxY, 0])
+            g[0, k - firstFrames] = np.mean(frame[minX:maxX, minY:maxY, 1])
+            b[0, k - firstFrames] = np.mean(frame[minX:maxX, minY:maxY, 2])
+    # print(k)
+    else:
+        break
+    k = k + 1
+
+cap.release()
+cv2.destroyAllWindows()
+
+r = r[0,0:length]-np.mean(r[0,0:length])
+g = g[0,0:length]-np.mean(g[0,0:length])
+b = b[0,0:length]-np.mean(b[0,0:length])
+
+# Use butter filter
+if True:
+    BPM_lowest = 40
+    BPM_max = 230
+    Wn = [((BPM_lowest/60)/fps * 2), ((BPM_max/60)/fps * 2)]
+    B_butter, A_butter = butter(N=2, Wn=Wn, btype='band')
+    r = filtfilt(B_butter, A_butter, r)
+    g = filtfilt(B_butter, A_butter, g)
+    b = filtfilt(B_butter, A_butter, b)
+
+fps = 30
+print("GREEN")
+tests.interval_test(g,fps)
+print("BLUE")
+tests.interval_test(b,fps)
--- tests/tests.py
+++ tests/tests.py
@@ -3,17 +3,38 @@ from fft import fft
 
 def interval_test(x,fps):
     total_frames = len(x)
-
     # amount of interval's frames
     ns = [512,1024,2048,4096]
-    for n in ns:
+    #info is a n dimensional array with avg, std, max, min in first 4
+    info = np.zeros((4, len(ns)))
+
+    for i in range(0,len(ns)):
+        n = ns[i]
         print("intervalo: ",n)
-        for i in range(0,int(total_frames/n)):
+        freqs = np.zeros(int(total_frames/n))
+        for j in range(0,int(total_frames/n)):
             if total_frames > n:
-                print(i)
-                frames = x[i*n:(i+1)*n - 1]
+                frames = x[j*n:(j+1)*n]
 
                 f = np.linspace(-n / 2, n / 2 - 1, n) * fps / n
                 X = np.abs(fft.FFT_shift(np.fft.fft(frames))) ** 2
-                print("\tFrecuencia cardíaca: ", abs(f[np.argmax(X)]) * 60, " pulsaciones por minuto")
+                freq = abs(f[np.argmax(X)]) * 60
+                print("\tFrecuencia cardíaca: ", freq, " pulsaciones por minuto")
+                freqs[j]=freq
 
+        if len(freqs)!=0:
+            info[0,i] = np.mean(freqs)
+            info[1,i] = np.std(freqs)
+            info[2,i] = np.max(freqs)
+            info[3,i] = np.min(freqs)
+
+
+    print('-----------')
+
+    for i in range(0, len(ns)):
+        print("Intervalo ", ns[i], ": ")
+        print("\tMedia: ", info[0][i])
+        print("\tDesvío: ", info[1][i])
+        print("\tMáximo: ", info[2][i])
+        print("\tMínimo: ", info[3][i])
+        print("\tAmplitud: ", info[2][i]-info[3][i])
